b651127428025873cb8f8f5d5e3d2c37d8d08af4
scotch1213 <84630273+scotch1213@users.noreply.github.com>
2021-09-08 23:22:11 +0900

    [210908]OpneGL Tutorial 7 commit!

--- OpenGL Tutorial/assets/fragment_core2.glsl
+++ OpenGL Tutorial/assets/fragment_core2.glsl
@@ -4,6 +4,6 @@ out vec4 FragColor;
 in vec4 color;
 
 void main(){
-	FragColor = vec4(1.0f, 0.2f, 0.6f, 1.0f);
+	FragColor = vec4(0.5f, 0.82f, 0.11f, 1.0f);
 	//FragColor = color;
 }
--- OpenGL Tutorial/assets/vertex_core.glsl
+++ OpenGL Tutorial/assets/vertex_core.glsl
@@ -1,6 +1,19 @@
 #version 330 core
 layout (location = 0) in vec3 aPos;
 
+out vec4 color;
+
 void main(){
 	gl_Position = vec4(aPos, 1.0);
+	/*
+	if(aPos.x < 0.0){
+		color = vec4(1.0, 0.0, 0.0, 1.0);
+	}
+	else if(aPos.x == 0){
+		color = vec4(0.0, 1.0, 0.0, 1.0);
+	}
+	else{
+		color = vec4(0.0, 0.0, 1.0, 1.0);
+	}
+	*/
 }
